6a17e57b106410f6369b9506cfca3802fbd15a88
charlielittle <charlie.little@mongodb.com>
2021-09-07 19:56:19 -0400

    Fixed async calls and batch view_desc inserts

--- src/index.js
+++ src/index.js
@@ -4,78 +4,114 @@ const url = "mongodb+srv://aod:aod@aod-views.qukk7.mongodb.net/aod?retryWrites=t
 const { strict } = require("assert");
 const { SSL_OP_EPHEMERAL_RSA } = require("constants");
 const { MongoClient } = require("mongodb");
+const { userInfo } = require("os");
+const { getSystemErrorMap } = require("util");
 const generate_index = require("../view_function");
 
-const client = new MongoClient(url);
+const client = new MongoClient(url, { "useUnifiedTopology": true });
 var count = 0;
 
 async function run() {
-  try {
-    await client.connect();
+	try {
+		await client.connect();
 
-    const database = client.db("aod");
-    const aod = database.collection("aod");
+		const database = client.db("aod");
+		const aod = database.collection("aod");
 
-	var result = await aod.findOne();
-	if( !result ) {
-		const fs = require('fs');
-		const path = require('path');
-		
-		let rawdata = fs.readFileSync(path.resolve(__dirname, '../aod-sample.json'));
-		let sample = JSON.parse(rawdata);
-		console.log(sample);
-		result = await aod.insertOne( sample );
-		if( result ) result = await aod.findOne();
+		var result = await aod.findOne();
+		if (!result) {
+			const fs = require('fs');
+			const path = require('path');
+
+			let rawdata = fs.readFileSync(path.resolve(__dirname, '../aod-sample.json'));
+			let sample = JSON.parse(rawdata);
+			console.log(sample);
+			result = await aod.insertOne(sample);
+			if (result) result = await aod.findOne();
+		}
+		delete result._id;
+		const viewFun = require('../view_function');
+		if (!result["requestInstance"]) {
+			result = { "requestInstance": result };
+		}
+		console.log(result);
+
+		var datascopes = await aod.distinct("requestInstance.datascopeId");
+		console.log(datascopes);
+		var view = database.collection("view_desc");
+		await createViews(datascopes, aod, view);
+		console.log("createViews returned.");
+		// console.log(result);
+		// insertMany( result, aod );
+	} catch( err ) {
+		console.error("Caught exception: " + err );
+	} finally {
+		while (count > 0) {
+			console.log(`waiting on background tasks: count=${count}`);
+			await new Promise(r => setTimeout(r, 2 * 1000));
+		}
+		await client.close();
+		console.log("Mongo session closed.  Exiting");
 	}
-	delete result._id;
-	const viewFun = require('../view_function');
-	if( !result["requestInstance"] ) {
-		result = { "requestInstance" : result };
-	}
-	console.log( result );
-	generate_index( result );
-	
-    // console.log(result);
-	insertMany( result, aod )
-  } finally {
-	while( count > 0 ) {
-		console.log( `waiting on background tasks: count=${count}` );
-		await new Promise( r => setTimeout( r,  2 * 1000 ));
-	}
-    await client.close();
-  }
 }
 run().catch(console.dir);
 
-async function insertMany( result, aod ) {
+async function createViews(datascopes, aod, view) {
+	// datascopes.forEach( function (scope) {
+	console.log( `datascopes to generate: ${datascopes.length}`);
+	for( let idx = 0; idx < datascopes.length; idx++ ) {
+		const scope = datascopes[ idx ];
+		// while (count >= 5) {
+		// 	console.log(`waiting on background tasks: count=${count}`);
+		// 	await new Promise(r => setTimeout(r, 2 * 1000));
+		// }
+		count++;
+		const docs = await aod.find({ "requestInstance.datascopeId": scope }).toArray();
+		console.log( `instances for datascope ${scope}: ${docs.length}`);
+		for( let i = 0; i < docs.length; i++ ) { //.forEach( async function(doc) {
+			const doc = docs[ i ];
+			const itemCount = await generate_index(doc, view);
+			count--;
+			console.log( `items added ${itemCount}` );
+		}// );
+			// count--;
+	} //);
+}
+
+async function insertMany(result, aod) {
 	var batch = [];
 	var id = result.requestInstance.id;
 	var datascopeId = result.requestInstance.datascopeId;
 	// need to remove _id if it is there
 	delete result._id;
-	for( let i = 0; i < 1000000; i++ ) {
+	for (let i = 0; i < 1000000; i++) {
 		let scopeIdx = Math.floor(i / 1000);
-		let scopePad = scopeIdx.toString().padStart( 5, "0" );
-		let padded = i.toString().padStart(9,"0");
-		result.requestInstance.id = id.concat( "-", padded );
-		result.requestInstance.datascopeId = datascopeId.concat( "-", scopePad );
-		if( i % 1013 == 0 )
+		let scopePad = scopeIdx.toString().padStart(5, "0");
+		let padded = i.toString().padStart(9, "0");
+		result.requestInstance.id = id.concat("-", padded);
+		result.requestInstance.datascopeId = datascopeId.concat("-", scopePad);
+		if (i % 1013 == 0)
 			console.log(result.requestInstance.id, result.requestInstance.datascopeId);
-		batch.push( JSON.parse( JSON.stringify(result) ) );
-		if( batch.length > 0 && batch.length % 1000 == 0 ) {
+		batch.push(JSON.parse(JSON.stringify(result)));
+		if (batch.length > 0 && batch.length % 1000 == 0) {
 			// insert batch async
-			console.log( `Batch length: ${batch.length}` );
+			console.log(`Batch length: ${batch.length}`);
 			count++;
-			const db = aod.insertMany( batch, { "ordered":false } ).then( (dbResult) => { 
-				console.log( dbResult) ;  
-				count--; 
+			const db = aod.insertMany(batch, { "ordered": false }).then((dbResult) => {
+				console.log(dbResult);
+				count--;
 				// console.log( (result._id||"no _id" )); 
-			} );
+			});
 			batch = [];
 		}
-		while( count > 5 ) {
-			console.log( `waiting on tasks before adding another: count=${count}` );
-			await new Promise( r => setTimeout( r,  2 * 1000 ));
+		while (count > 5) {
+			console.log(`waiting on tasks before adding another: count=${count}`);
+			await new Promise(r => setTimeout(r, 2 * 1000));
 		}
 	}
+}
+
+async function sleep(secs) {
+	await new Promise(r => setTimeout(r, secs * 1000));
+	console.log("slept " + secs);
 }
--- view_function.js
+++ view_function.js
@@ -1,45 +1,74 @@
-const generate_index = function (doc, meta) {
-  var dataScope = doc.requestInstance.datascopeId;
-  var objectType =  doc.requestInstance.objectType;
-  var id = doc.requestInstance.id;
+const generate_index = async function (doc, view) {
+	var dataScope = doc.requestInstance.datascopeId;
+	var objectType = doc.requestInstance.objectType;
+	var id = doc.requestInstance.id;
+	var items = 0;
+	var batch = [];
 
-  if(dataScope && objectType){
-    if(objectType !="JOB"){
+	if (dataScope && objectType) {
+		if (objectType != "JOB") {
+			var processChildren = async function (dataScope, logicalDimId, children) {
+				if (children == null || children.length == 0)
+					return;
 
-      var processChildren = function(dataScope,logicalDimId,children)
-      {
-        if(children == null || children.length == 0)
-          return;
+				for (var k = 0; k < children.length; k++) {
+					child = children[k];
+					// emit([dataScope,logicalDimId,child.type,child.desc,objectType],null);
+					let item = { "id": id, "datascopeId": dataScope, "logicalDimId": logicalDimId, "type": child.type, "desc": child.desc, "objectType": objectType };
+					try {
+						batch.push( item );
+						// let res = await view.insertOne( item ); //.then(res => items++, (err) => { 
+						// console.log( `inserted: ${JSON.stringify(item)}`);
+						items++;
+					} catch( err ) {
+						console.error( "desc view insert err: " + err );
+						if( err.code = 11000 ) {
+							console.log( "Duplicate: " + JSON.stringify( item ) );
+							return;
+						}
+					}
+					//   items++;
+					//   console.log( item );
 
-        for(var k=0;k<children.length; k++)
-        {
-          child = children[k];
-          // emit([dataScope,logicalDimId,child.type,child.desc,objectType],null);
-		  let item = { "id" : id, "dataScopeId": dataScope,"logicalDimId":logicalDimId,"type":child.type,"desc":child.desc,"objectType":objectType };
-        //   let res = db.view_desc.insertOne( item );
-          console.log( item );
+					// emit([dataScope,logicalDimId,child.type,child.nodeId,objectType],null);
+					item = { "id": id, "datascopeId": dataScope, "logicalDimId": logicalDimId, "type": child.type, "nodeId": child.nodeId, "objectType": objectType };
+					try {
+						batch.push( item );
+						// let res = await view.insertOne( item ); //.then(res => items++, (err) => { 
+						// console.log( `inserted: ${JSON.stringify(item)}`);
+						items++;
+					} catch( err ) {
+						console.error( "nodeId view insert err: " + err );
+						if( err.code = 11000 ) {
+							console.log( "Duplicate: " + JSON.stringify( item ) );
+							return;
+						}
+					}
+					//   items++;
+					//    console.log( item );
+					await processChildren(dataScope, logicalDimId, children[k].children);
+				}
+			}
 
-		  // emit([dataScope,logicalDimId,child.type,child.nodeId,objectType],null);
-		  item =     { "id" : id, "dataScopeId": dataScope,"logicalDimId":logicalDimId,"type":child.type,"nodeId":child.nodeId,"objectType":objectType};
-        //   res = db.view_desc.insertOne( item );
-          console.log( item );
-          processChildren(dataScope,logicalDimId,children[k].children);
-        }
-      }
-
-      for(var h=0;h<doc.requestInstance.dataSet.length;h++){
-        var logicalDimId = doc.requestInstance.dataSet[h].logicalDimId;
-        for(var i=0;i<doc.requestInstance.dataSet[h].selections.length;i++)
-           {
-            for(var j=0;j<doc.requestInstance.dataSet[h].selections[i].items.length;j++)
-                {
-                 var itemzz = doc.requestInstance.dataSet[h].selections[i].items[j];
-                  processChildren(dataScope,logicalDimId,itemzz.children);
-                 }
-            }
-       }
-    }
-  }
+			for (var h = 0; h < doc.requestInstance.dataSet.length; h++) {
+				var logicalDimId = doc.requestInstance.dataSet[h].logicalDimId;
+				for (var i = 0; i < doc.requestInstance.dataSet[h].selections.length; i++) {
+					for (var j = 0; j < doc.requestInstance.dataSet[h].selections[i].items.length; j++) {
+						var itemzz = doc.requestInstance.dataSet[h].selections[i].items[j];
+						await processChildren(dataScope, logicalDimId, itemzz.children);
+					}
+				}
+			}
+			if( batch.length > 0 ) {
+				let res = await view.insertMany( batch, { ordered: false, writeConcern: {w:1} });
+				console.log( `insertMany: ${res}` );
+			} else {
+				console.log( `Empty batch` );
+			}
+			console.log( `${items} inserted for id: ${id} datascope: ${dataScope} objectType: ${objectType}` );
+		}
+	}
+	return items; // return count of inserted index entries
 }
 
 module.exports = generate_index;
